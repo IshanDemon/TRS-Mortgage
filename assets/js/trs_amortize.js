4634f928f6bb6588aad6525b72f87147c8a79dd8
raj-uimatic <raj-uimatic@gmail.com>
2016-05-04 18:25:07 +0530

    adding intial data

---
+++ assets/js/trs_amortize.js
@@ -0,0 +1,77 @@
+/*
+ * Js to calculate amortization data
+ */
+
+
+function pmt(rate, nper, pv) {
+    var pvif, pmt;
+
+    pvif = Math.pow(1 + rate, nper);
+    console.log(pvif);
+    pmt = rate / (pvif - 1) * -(pv * pvif);
+
+    return pmt;
+}
+;
+
+// for bi-weekly payments_per_year = 26
+function computeSchedule(loan_amount, interest_rate, payments_per_year, years, payment) {
+//    var schedule = {};
+    var schedule = [];
+    var remaining = loan_amount;
+    var number_of_payments = payments_per_year * years;
+    var mainDate = new Date();
+    for (var i = 0; i < number_of_payments; i++) {
+        var interest = remaining * (interest_rate / 100 / payments_per_year);
+        var principle = (payment - interest);
+        var paymentDate = (i === 0) ? new Date(mainDate) : new Date(addDaysInDate(mainDate, payments_per_year));
+        principle = principle > 0 ? (principle < payment ? principle : payment) : 0;
+        interest = interest > 0 ? interest : 0;
+        var newRemainingAmount = remaining > 0 ? remaining - principle : 0;
+        var row = [
+            paymentDate,
+            principle,
+            interest,
+            getTotalAmountPaid(principle, interest),
+            newRemainingAmount,
+            loanPaidTillDate(loan_amount, newRemainingAmount)
+        ];
+        // create a year key in schedule array. Which will store all the data for that year
+//        if (typeof schedule[paymentDate.getFullYear()] == 'undefined') {
+//            schedule[paymentDate.getFullYear()] = {};
+//            schedule[paymentDate.getFullYear()].data = [];
+//        }
+//        try {
+//            schedule[paymentDate.getFullYear()].data.push(row);
+//        }
+//        catch (err) {
+//            schedule[paymentDate.getFullYear()].data = [];
+//            schedule[paymentDate.getFullYear()].data.push(row);
+//        }
+        schedule.push(row);
+        remaining -= principle;
+    }
+    console.log(schedule);
+    return schedule;
+}
+
+function addDaysInDate(date, payments_per_year) {
+    if (payments_per_year === 26) {
+        return date.setDate(date.getDate() + 14);
+    } else {
+        return date.setMonth(date.getMonth() + 1);
+    }
+}
+
+/*
+ * This function returns loan paid in percentage
+ */
+function loanPaidTillDate(loan_amount, newBalance) {
+    return ((parseFloat(loan_amount) - parseFloat(newBalance)) / loan_amount) * 100;
+}
+/*
+ * This function returns total amount paid in any date
+ */
+function getTotalAmountPaid(principle, interest) {
+    return  parseFloat(principle) + parseFloat(interest); // to calculate total amount paid
+}
---
+++ assets/js/trs_calculator.js
@@ -0,0 +1,87 @@
+/*
+ * Js to calculate amortization data
+ */
+(function () {
+
+    function getDataSet() {
+        var output = {};
+        var loanAmount = output.loanAmount = parseFloat($('#loanAmount').val());
+        var interestRate = output.interestRate = parseFloat($('#interestRate').val());
+        var paymentsPerYear = output.paymentsPerYear = parseInt($('#paymentsPerYear').val());
+        var years = output.years = parseInt($('#years').val());
+        var numberOfPayments = output.numberOfPayments = paymentsPerYear * years;
+console.log("interestRate / 100 / paymentsPerYear");
+console.log(interestRate / 100 / paymentsPerYear);
+        var payment = output.payment = pmt(interestRate / 100 / paymentsPerYear, numberOfPayments, -loanAmount);
+//        var payment = output.payment = pmt(interestRate, numberOfPayments, loanAmount);
+
+        output.schedule = computeSchedule(loanAmount,
+                interestRate,
+                paymentsPerYear,
+                years,
+                payment);
+        return output;
+    }
+
+
+    function reloadTable(ds) {
+        // map the schedule to 2 digits after decimal point.
+        var schedule = ds.schedule.map(function (n) {
+            return [n[0], n[1].toFixed(2), n[2].toFixed(2), n[3].toFixed(2), n[4].toFixed(2),n[5].toFixed(2)+"%"];
+        });
+        $('#schedule').empty();
+        $('#schedule').html('<table cellpadding="0" cellspacing="0" border="0" class="table" id="schedule_table"></table>');
+        $('#schedule_table').dataTable({
+            "data": schedule,
+            "searching": false,
+            "columns": [
+                {"title": "Period"},
+                {"title": "Principle"},
+                {"title": "Interest"},
+                {"title": "Total"},
+                {"title": "Remaining"},
+                {"title": "Loan Paid To Date"}
+            ],
+            "search": false,
+            "paging": true,
+            "ordering": false,
+            "info": false
+        });
+    }
+
+    function reloadGraph(ds) {
+        var graphWidth = $('#outline-container-sec-2').width(); // make graph same width as table
+        var graphHeight = $('#graph').height();
+        var periodWidth = Math.round(graphWidth / (ds.numberOfPayments));
+
+        // adjust graphy width for rounding of period width
+        graphWidth = periodWidth * ds.numberOfPayments;
+
+        $('#graph').empty();
+        $('#graph').width(graphWidth);
+
+        for (var count = 0; count < ds.numberOfPayments; count++) {
+            var i = ds.schedule[count][1];
+            var p = ds.schedule[count][2];
+            var t = i + p;
+            var ratio = i / t;
+            var height = Math.round(graphHeight * ratio);
+            $('<div style="float: left; margin-top: ' + (graphHeight - height).toString() + 'px; background-color: yellow; height: ' + height + 'px; width: ' + periodWidth + 'px"></div>').appendTo('#graph');
+        }
+    }
+
+    function reload() {
+        var ds = getDataSet();
+        $('#paymentAmount').text('$' + ds.payment.toFixed(2));
+        reloadTable(ds);
+        reloadGraph(ds);
+    }
+
+
+    $(document).on('keyup', '.user-input', reload);
+
+    $(document).ready(function () {
+        reload();
+    });
+
+})();
